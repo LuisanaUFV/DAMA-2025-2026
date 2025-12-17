8df54c63ce1377b92e4502bdcb1a86ec40d50e46
LuisanaUFV <luisana.rodriguez@ufv.es>
2025-12-17 09:43:04 +0100

    Utilizamos Scanner

--- src/ejemplo01/Sueldo.java
+++ src/ejemplo01/Sueldo.java
@@ -5,24 +5,30 @@ package ejemplo01;
     * impuestos.
     */
 
+import java.util.Scanner;
+
 public class Sueldo {
 
 	public static void main(String[] args) {
         // ZONA DE DATOS: Declaración e inicialización de constantes y variables
 		// Datos entrada
 		int sueldo = 0;
+		Scanner teclado = new Scanner(System.in);
 		
 		// Datos de salida
 		
 		
 		// ZONA DE INSTRUCCIONES
-		// Simulamos leer dato de entrada
-		sueldo = 3000;
+		// Trabajamos ya con Scanner
+		//sueldo = 3000;
+		System.out.println("Por favor, introduce el sueldo:");
+		sueldo = teclado.nextInt();
 		
 		// Condición
 		if ( sueldo >= 3000 ) {
 	      System.out.println("Debes pagar impuestos!!!!");		
 	    }
+		teclado.close();
 	    System.out.println("Bye!!!!");
 	}
 
